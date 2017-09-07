9b931e9ca78558dd6cbc2d8be37f1164f647c659
heuulzp <heuulzp@163.com>
2017-09-07 18:14:00 +0800

    避免状态单例

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-// 第一步：创建一个实例
-const Vue = require('vue');
-const app = new Vue({
-  template: '<div>Hello World</div>'
-})
-// 第二步：创建一个renderer
-const renderer = require('vue-server-renderer').createRenderer()
-// 将Vue实例渲染为HTML
-renderer.renderToString(app, (err, html) => {
-  if (err) {
-    throw err
-  }
-  console.log(html);
-})
+const Vue = require('vue')
+
+module.exports = function createApp (context) {
+  return new Vue({
+    data: {
+      url: context.url
+    },
+    template: '<div>访问的 URL 是：{{ url }}</div>'
+  })
+}
--- server.js
+++ server.js
@@ -1,31 +1,27 @@
 const Vue = require('vue');
+const createApp = require('./app')
 const server = require('express')();
-const renderer = require('vue-server-renderer').createRenderer();
+const renderer = require('vue-server-renderer').createRenderer({
+  template: require('fs').readFileSync('./mustache.template.html', 'utf-8')
+});
 
 server.get('*', (req, res) => {
-  const app = new Vue({
-    data: {
-      url: req.url
-    },
-    template: '<div>访问的url是{{url}}</div>'
-  });
-  renderer.renderToString(app, (err, html) => {
+
+  const context = {
+    title: 'hello',
+    meta: `<meta charset='utf-8'>`,
+    url: req.url
+  }
+
+  const app = createApp(context)
+
+  renderer.renderToString(app, context, (err, html) => {
     if (err) {
       res.status(500).end('Internal Server Error')
       return
     }
     res.setHeader('Content-Type','text/html;charset=UTF-8'); // 避免乱码
-    res.end(`
-      <!DOCTYPE html>
-      <html lang="en">
-        <head>
-          <title>Hello</title>
-        </head>
-        <body>
-          ${html}
-        </body>
-      </html>
-    `);
+    res.end(html)
   });
 })
 
